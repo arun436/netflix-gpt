5f8f7487e9f63b4cde8d5f2608ff7458e8837214
Arun Kumar Chintha <arunkumarchintha136@gmail.com>
2024-03-18 21:17:33 +0530

    Sign In/Sign Up && Login/Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import Body from "./components/Body";
+
 function App() {
-  return (
-    <div className="font-bold text-3xl text-green-800">
-      Namaste Everyone. Let's build Netflix GPT
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignIn(!isSignIn);
+  };
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          className="absolute"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="bg"
+        />
+      </div>
+      <form className="w-3/12 p-12 bg-black absolute my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignIn && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button type="submit" className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p
+          className="py-4 cursor-pointer hover:underline"
+          onClick={toggleSignInForm}
+        >
+          {isSignIn ? "New to Netflix? Sign Up Now" : "Already a user? Sign In"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
